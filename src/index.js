2e42785048c530362fe9a690249ad89ef006f27c
steven2k2 <steven.thompson2k24@gmail.com>
2025-01-31 20:47:29 +1100

    feature: added handlebars support.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
 import './styles/main.scss'
-import coding from './assets/coding.svg'
-
-const imageTag = document.getElementById('codeImg')
-imageTag.src = coding
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,52 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    bundle: path.resolve(__dirname, 'src/index.js')
-  },
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
+    filename: '[name].[contenthash].js',
     clean: true,
-    assetModuleFilename: '[name][ext]'
+    assetModuleFilename: '[name][ext]',
   },
   devtool: 'source-map',
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist')
-    },
-    hot: true, // Enable webpack's Hot Module Replacement feature:
+    static: path.resolve(__dirname, 'dist'),
+    hot: true,
     open: true,
-    compress: true, // Enable gzip compression for everything served:
-    port: 3000,
-    historyApiFallback: true // TODO
+    compress: true,
+    port: 3010,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
-        test: /\.scss$/, // loads sass
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        test: /\.ejs$/,
+        loader: 'ejs-loader',
+        options: { esModule: false },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
+        options: { presets: ['@babel/preset-env'] },
       },
       {
         test: /\.(png|svg|jpg|gif)$/i,
-        type: 'asset/resource'
-      }
-    ]
+        type: 'asset/resource',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack + EJS',
       filename: 'index.html',
-      template: path.resolve(__dirname, 'src/template.html')
-    })
-  ]
-}
+      template: path.resolve(__dirname, 'src/template.ejs'),
+    }),
+  ],
+};
